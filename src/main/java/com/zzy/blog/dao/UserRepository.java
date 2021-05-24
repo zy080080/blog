6a9b3f984711719a5b02fc939bbee0f1bfbe32d4
zzy <44433739+zy080080@users.noreply.github.com>
2021-05-24 16:20:02 +0900

    Delete src directory

--- src/main/java/com/zzy/blog/dao/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.zzy.blog.dao;
-
-import com.zzy.blog.po.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsernameAndPassword(String username, String password);
-}
--- src/main/java/com/zzy/blog/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.zzy.blog.service;
-
-import com.zzy.blog.po.User;
-
-public interface UserService {
-    User checkUser(String username, String password);
-}
